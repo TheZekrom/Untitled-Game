fa02f120532d5b936d66057cb83302bf46846085
TheZekrom <iwanjeffery1990@gmail.com>
2014-02-16 18:43:28 +0000

    Moved code

--- Untitled Game/Main.cpp
+++ Untitled Game/Main.cpp
@@ -1,37 +1,34 @@
-#include <iostream>
-#include <SDL.h>
+#include "stdafx.h"
+#include "Main.h"
 
-int main(int argc, char **argv){
 
-	bool quit = nullptr;
-	
-	SDL_Window* window = nullptr;
-	SDL_Renderer* renderer = nullptr;
-	SDL_Event* event = new SDL_Event();
-
-	SDL_Init(SDL_INIT_VIDEO);
-
+CMain::CMain()
+{
+	quit = false;
+	window = nullptr;
+	renderer = nullptr;
 	window = SDL_CreateWindow("First Game", 100, 100, 1280, 720, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	event = new SDL_Event();
+	if (window == nullptr){std::cout << "Window Failed to Instailize";quit = 1;}
+}
 
-	if (window == nullptr){
-		std::cout << "Window Failed to Instailize";
-		return 0;
-	}
 
-	
-	while (!quit && event-> type != SDL_QUIT)
-	{
-		SDL_PollEvent(event);
-		SDL_RenderClear(renderer);
-
-		SDL_RenderPresent(renderer);
-	}
-
+CMain::~CMain()
+{
 	delete event;
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
-	
+}
 
-	return 0;
+void CMain::GameLoop(void)
+{
+	while (!quit && event->type != SDL_QUIT)
+	{
+		SDL_PollEvent(event);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+
 }
