8e75a88341a75228bfa3d8e148f39c7b31b2c955
TheZekrom <iwanjeffery1990@gmail.com>
2014-02-17 12:47:31 +0000

    Better code management, easier sprite drawing

--- Untitled Game/Main.cpp
+++ Untitled Game/Main.cpp
@@ -1,11 +1,17 @@
 #include "stdafx.h"
 #include "Main.h"
 
+#include "StdAfx.h"
+#include "Main.h"
 
-CMain::CMain()
+CMain::CMain(int passed_ScreenWidth, int passed_ScreenHeight)
 {
+	ScreenWidth = passed_ScreenWidth;
+	ScreenHeight = passed_ScreenHeight;
 	quit = false;
-	csdl_setup = new CSDL_Setup(&quit);
+	csdl_setup = new CSDL_Setup(&quit, ScreenWidth, ScreenHeight);
+	grass = new CSprite(csdl_setup->GetRenderer(), "grass.bmp", 0, 0, ScreenWidth, ScreenHeight);
+	bob = new CSprite(csdl_setup->GetRenderer(), "bob.png", 300, 250, 50, 100);
 }
 
 
@@ -19,7 +25,12 @@ void CMain::GameLoop(void)
 	while (!quit && csdl_setup->GetEvent()->type != SDL_QUIT)
 	{
 		csdl_setup->Begin();
-		SDL_RenderPresent(csdl_setup->GetRenderer());
+
+		grass->Draw();
+
+		bob->Draw();
+
+		csdl_setup->End();
 	}
 
 
--- Untitled Game/Main.h
+++ Untitled Game/Main.h
@@ -1,14 +1,24 @@
 #pragma once
 #include "SDL_Setup.h"
+#include "Sprite.h"
+
 class CMain
 {
 public:
-	CMain();
-	~CMain();
+	CMain(int passed_ScreenWidth, int passed_ScreenHeight);
+	~CMain(void);
 	void GameLoop();
 
 private:
-	bool quit;
-	CSDL_Setup*	csdl_setup;
-};
+	int ScreenWidth;
+	int ScreenHeight;
 
+	bool quit;
+
+	CSprite* grass;
+
+	CSprite* bob;
+
+	CSDL_Setup* csdl_setup;
+
+};
